7400e6f8c489be54ba4dd35c565d9fa547016862
ahayn465 <ahayn465@mtroyal.ca>
2016-11-02 11:55:05 -0600

    inital commit - databas connected

---
+++ beer_me/Brewery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SQLite;
+
+namespace beer_me
+{
+	public class Brewery
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+
+		private string Name { get; set; }
+
+		private string Address { get; set; }
+
+		private string Phone { get; set; }
+
+		private int latitude { get; set; } 
+
+		private int longitude { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("[Person: ID={0}, Name={1}, Address={2}]", ID, Name, Address);
+		}
+	}
+}
---
+++ beer_me/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using SQLite;
+
+namespace beer_me
+{
+	[Activity(Label = "beer_me", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+
+            // create DB path
+            var docsFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+			var pathToDatabase = System.IO.Path.Combine(docsFolder, "db_sqlnet.db");
+
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			TextView database = FindViewById<TextView>(Resource.Id.databaseConnectionTester);
+
+			database.Text = string.Format("waiting...");
+
+			var result = createDatabase(pathToDatabase);
+			Console.WriteLine("Database connected with {0} result", result);
+
+		}
+
+		private string createDatabase( string path )
+		{
+			try
+			{
+				var connection = new SQLiteAsyncConnection(path);
+				connection.CreateTableAsync<Brewery>();
+
+				TextView database = FindViewById<TextView>(Resource.Id.databaseConnectionTester);
+				database.Text = string.Format("Database connected");
+				return "Database created";
+				
+			}
+			catch(SQLiteException ex)
+			{
+				return ex.Message;
+			}
+		}
+
+
+	}
+}
+
